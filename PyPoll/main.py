bf028a8f45576dfd7b3cbcf6fa5a0a1a3bf48cf2
Annabel Cheong <annabelcheong@192-168-1-100.tpgi.com.au>
2020-12-31 13:14:54 +0800

    Updated directory paths and comments in PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,11 +3,10 @@ import os
 import csv
 
 #Create file path and join
-#print(os.path.abspath(os.curdir))
-# os.chdir("..")
-#print(os.path.abspath(os.curdir))
+#TEST: print(os.path.abspath(os.curdir))
+#TEST: os.chdir("..")
+#TEST: print(os.path.abspath(os.curdir))
 csvpath = os.path.join("Resources/PyPoll_data.csv")
-#csvpath = os.path.join("/Users/annabelcheong/Documents/Data_Analytics/python-challenge/PyPoll/Resources/PyPoll_data.csv")
 with open(csvpath, newline="") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     header = next(csv_reader)
