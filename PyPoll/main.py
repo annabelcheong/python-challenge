e06ce294a0a9d6cab5b39788a8355b896a2d6e67
Annabel Cheong <annabelcheong@192-168-1-100.tpgi.com.au>
2020-12-31 11:40:57 +0800

    Comments edited and wrote summary report to text file on python code. Code completed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -21,7 +21,7 @@ with open(csvpath, newline="") as csv_file:
 
     # print(int(line_count))
 
-#Create and store Voter ID, County, Candidate data into lists
+    #Create and store Voter ID, County, Candidate data into lists
     voter_id_string=[]
     county=[]
     candidate=[]
@@ -31,49 +31,15 @@ with open(csvpath, newline="") as csv_file:
         county.append(row[1])
         candidate.append(row[2])
 
-    #Print the number of voters
+    ####FIND AND PRINT THE NUMBER OF TOTAL VOTERS####
     voter_id = list(map(int, voter_id_string))
-    #print(voter_id)
+    #TEST: print(voter_id)
     total_voters=len(voter_id)
     print("\nElection Results\n\n------------------------")
     print(f"Total Votes:{len(voter_id)}")
     print("------------------------")
 
-    from collections import Counter
-    # candidate_counter=Counter(candidate)
-    # print(candidate_counter)
-    
-    # print(Counter(candidate_counter.keys()))
-    # print(Counter(candidate_counter.values()))
-
-    #Find and Print the winning candidate
-    # common_candidate=candidate_counter.most_common(1)
-    # print(f"Winner:{common_candidate}")
-
-    vote_count=Counter(candidate)
-    print(f"vote count: {vote_count}")
-    max_votes=max(vote_count.values())
-    print(f"max votes: {max_votes}")
-
-    #Search for candidate with max votes and store in list
-    First=[i for i in vote_count.keys() if vote_count[i]==max_votes]
-    print(sorted(First)[0])
-    print(vote_count.keys)
-
-    common_candidate=vote_count.most_common(1)
-    print(f"Most Common Method:{common_candidate}")
-    print(list(i for i in common_candidate.keys()))
-    print(common_candidate[0])
-   #Search for candidate with max votes and store in list
-    First=[i for i in vote_count.keys()]
-    #print(sorted(First)[0])
-    print(sorted(First))
-    print(vote_count.keys())
-
-
-
-
-
+    ####CALCULATE AND PRINT THE % AND NUMBER OF VOTES FOR EACH CANDIDATE####
 
     #Count up total votes and % for Khan 
     khan_count=candidate.count("Khan")
@@ -87,18 +53,35 @@ with open(csvpath, newline="") as csv_file:
     li_count=candidate.count("Li")
     print(f"Li: {li_count/total_voters*100:.2f}% ({li_count})")
 
+    #Count up total votes and % for O'Tooley
     otooley_count=candidate.count("O'Tooley")
     print(f"O'Tooley: {otooley_count/total_voters*100:.2f}% ({otooley_count})")
 
-
-
+    ####FIND WINNER####
+    from collections import Counter
    
-    # result_set=set(candidate)
-    # results={}
-    # for i in result_set:
-    #     results[i]=candidate.count(i)
-    # print(results)
-    # print(type(results))
+    vote_count=Counter(candidate)
+    #TEST: print(f"vote count: {vote_count}")
+    max_votes=max(vote_count.values())
+    #TEST: print(f"max votes: {max_votes}")
 
+    #Search for candidate with max votes and store in list
+    First=[i for i in vote_count.keys() if vote_count[i]==max_votes]
+    print("------------------------")
+    print(f"Winner: {sorted(First)[0]}")
+    print("------------------------")
 
-   
+   ####CREATE SUMMARY REPORT AND EXPORT RESULT TO TEXT FILE####
+
+report=open('PyPoll_Summary.txt','w')
+report.write("Election Results\n\n------------------------\n")
+report.write(f"Total Votes:{len(voter_id)}\n")
+report.write("------------------------\n")
+report.write(f"Khan: {khan_count/total_voters*100:.2f}% ({khan_count})\n")
+report.write(f"Correy: {correy_count/total_voters*100:.2f}% ({correy_count})\n")
+report.write(f"Li: {li_count/total_voters*100:.2f}% ({li_count}\n")
+report.write(f"O'Tooley: {otooley_count/total_voters*100:.2f}% ({otooley_count})\n")
+report.write("------------------------\n")
+report.write(f"Winner: {sorted(First)[0]}\n")
+report.write("------------------------")
+report.close()
